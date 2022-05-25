29ccaeba37b2a831ea1a0b6a8548673a32fbc3c1
Michael Hallquist <michael.hallquist@gmail.com>
2022-05-25 08:49:03 -0400

    schaefer 400 mapping

--- map_17_7.R
+++ map_17_7.R
@@ -4,23 +4,23 @@ setwd("~/Data_Analysis/schaefer_wb_parcellation")
 # To circumvent this, we lookup up the CoM of each ROI, then sort the dataset on x,y,z and assign a spatial_roi_num that matches between orders
 # The spatial_roi_num then forms the merging/identifying column
 
-schaefer_7 <- read.csv("labels/Schaefer2018_200Parcels_7Networks_order.csv") %>%
+schaefer_7 <- read.csv("labels/Schaefer2018_400Parcels_7Networks_order.csv") %>%
   mutate(network=factor(network), net_num = as.numeric(network)) %>%
   rename(network7=network, net_num7=net_num)
 
 # this has the spatial coordinate, spatial_roi_num
-schaefer_7_lookup <- read.csv("labels/Schaefer_200_7networks_labels.csv")
+schaefer_7_lookup <- read.csv("labels/Schaefer_400_7networks_labels.csv")
 
 schaefer_7 <- schaefer_7 %>% inner_join(schaefer_7_lookup, by="roi_num") %>%
   rename(roi_num7=roi_num, subregion7=subregion)
 
-schaefer_17 <- read.csv("labels/Schaefer2018_200Parcels_17Networks_order.csv") %>%
+schaefer_17 <- read.csv("labels/Schaefer2018_400Parcels_17Networks_order.csv") %>%
   mutate(network=factor(network), net_num = as.numeric(network)) %>%
   rename(network17=network, net_num17=net_num) %>%
   select(-hemi) # mirrored in 7
 
 # this has the spatial coordinate, spatial_roi_num
-schaefer_17_lookup <- read.csv("labels/Schaefer_200_17networks_labels.csv") %>%
+schaefer_17_lookup <- read.csv("labels/Schaefer_400_17networks_labels.csv") %>%
   select(roi_num, spatial_roi_num) # x,y,z and labels already duplicated in 7-network lookup
 
 schaefer_17 <- schaefer_17 %>% inner_join(schaefer_17_lookup, by="roi_num") %>%
@@ -29,6 +29,12 @@ schaefer_17 <- schaefer_17 %>% inner_join(schaefer_17_lookup, by="roi_num") %>%
 both <- inner_join(schaefer_7, schaefer_17, by="spatial_roi_num") %>%
   select(spatial_roi_num, roi_num7, roi_num17, network7, network17, net_num7, net_num17, subregion7, subregion17, everything())
 setDT(both)
+
+both %>% filter(network7 == "DorsAttn" | network17 %in% c("DorsAttnA", "DorsAttnB")) %>% 
+  pull(roi_num7) %>%
+  diff()
+
+
 
 
 fill_mask_with_stats(mask_nifti="Schaefer_244_final_2009c_2.3mm.nii.gz", 
@@ -48,14 +54,20 @@ both %>% group_by(network17) %>% filter(row_number()==1) %>% arrange(net_num17) 
 # 400 parcel attempt
 schaefer_7 <- read.csv("labels/Schaefer2018_400Parcels_7Networks_order.csv") %>%
   mutate(network=factor(network), net_num = as.numeric(network)) %>%
-  rename(network7=network, net_num7=net_num)
+  rename(network7=network, net_num7=net_num, subregion7=subregion)
 
 schaefer_17 <- read.csv("labels/Schaefer2018_400Parcels_17Networks_order.csv") %>%
   mutate(network=factor(network), net_num = as.numeric(network)) %>%
-  rename(network17=network, net_num17=net_num)
+  rename(network17=network, net_num17=net_num, subregion17=subregion)
 
-both <- inner_join(schaefer_7, schaefer_17, by="roi_num")
+# DANGER: don't merge by roi_num since the orders are different... use spatial_roi_num above!
+#both <- inner_join(schaefer_7, schaefer_17, by="roi_num")
 setDT(both)
+
+both %>% filter(network7 == "DorsAttn" | network17 %in% c("DorsAttnA", "DorsAttnB")) %>% 
+  pull(roi_num) %>%
+  diff()
+
 fill_mask_with_stats(mask_nifti="high_res_originals/Schaefer2018_400Parcels_7Networks_order_FSLMNI152_1mm_ants.nii.gz", 
                      stat_dt = both, mask_col = "roi_num", 
                      stat_cols = c("net_num7", "net_num17", "roi_num"),
@@ -80,3 +92,30 @@ fill_mask_with_stats(mask_nifti="high_res_originals/Schaefer2018_200Parcels_Kong
                      stack_along = "stat_cols", img_prefix = "schaefer200_17_kong", out_dir = "~/Downloads/dan_labels",
                      afni_dir = "~/abin", overwrite = T)
 
+
+
+
+
+# just testing the ability to pass expressions to R_batch_job, rather than character vectors
+# ee <- expression(
+#   x <- 15,
+#   for (i in 1:10) { print(i)}
+# )
+# 
+# x <- expression({
+#   hello <- 5
+#   for (i in 1:10) {
+#     print(i)
+#   }
+# })
+# 
+# r_code = quote({
+#   f <- rbindlist(lapply(sub_data$file, fread))
+#   setkeyv(a_coordinates, c('x', 'y', 'z'))
+#   setkeyv(f, c('x', 'y', 'z'))
+#   f <- merge(f, a_coordinates)
+#   f[, atlas_name := 'Schaefer_400_remap']
+#   f[, atlas_value := NULL]
+#   out_file <- file.path('/proj/mnhallqlab/users/michael/sceptic_decon/Schaefer_400_remap/deconvolved', paste0('sub', sub_data$sub[1L], '_run', sub_data$run[1L], '_Schaefer_400_remap_2.3mm_deconvolved.csv.gz'))
+#   fwrite(f, file=out_file)
+# })
